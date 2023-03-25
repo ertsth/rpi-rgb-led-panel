7ee442a5f8b39d1cabb086068e40f5e199788289
Dmytro Holub <holubdmytro11@gmail.com>
2023-03-25 15:51:30 +0200

    Use lua modes concept in main

--- main.cpp
+++ main.cpp
@@ -1,25 +1,14 @@
-#include "src/Color.h"
-#include "src/LedMatrix.h"
+#include "src/ModeManager.h"
 
 #include <thread>
 #include <chrono>
 
 using namespace std::chrono_literals;
-using rgb_matrix::RGBMatrix;
 
 int main(int argc, char **argv) {
 
-    LedMatrix* matrix = new LedMatrix();
-    matrix->fill(*(new Color(0, 255, 0)));
-    matrix->setPixel(10, 10, *(new Color(255, 255, 0)));
-    // std::this_thread::sleep_for(10000ms);
-    // matrix->clear();
-    std::this_thread::sleep_for(5000ms);
+    ModeManager* mm = new ModeManager();
+    mm->startMode(ModeManager::MODE::HELLO);
 
-    matrix->clear();
-    matrix->fill(*(new Color(255, 255, 0)));
-    std::this_thread::sleep_for(1000ms);
-
-    delete matrix;
     return 0;
 }
